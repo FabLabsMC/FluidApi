fb07d4911d0fb0991d642387139dc149f8abc5dc
Devan-Kerman <dev.sel20@gmail.com>
2020-05-23 17:36:58 -0500

    Update src/main/java/io/github/fablabsmc/fablabs/api/fluidvolume/v1/volume/vanilla/BucketFluidVolume.java
    Co-authored-by: i509VCB <i509vcb@gmail.com>

--- src/main/java/io/github/fablabsmc/fablabs/api/fluidvolume/v1/volume/vanilla/BucketFluidVolume.java
+++ src/main/java/io/github/fablabsmc/fablabs/api/fluidvolume/v1/volume/vanilla/BucketFluidVolume.java
@@ -9,6 +9,6 @@ import net.minecraft.item.Items;
 
 public class BucketFluidVolume extends ImmutableFluidVolume /*unfortunately, due to the nature of items in mc and how potions are implemented, it must be immutable*/ {
 	public BucketFluidVolume(ItemStack buckets) {
-		super(((BucketItemAccess) buckets.getItem()).getFluid(), (buckets.getItem() == Items.BUCKET ? Fraction.ZERO : Fraction.ONE).multiply(buckets.getCount()), buckets.getSubTag("fluid_data"));
+		super(((BucketItemAccess) buckets.getItem()).getFluid(), (buckets.getItem() == Items.BUCKET ? Fraction.ZERO : Fraction.ONE).multiply(buckets.getCount()), buckets.getOrCreateSubTag("fluid_data"));
 	}
 }
